427a5a9b24a74e184c79d4f7ab990adf9ef3fdc2
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-03 19:48:22 -0400

    started Critter app deleted a bunch of things

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import HomeFeed from './HomeFeed';
+import Notifications from './Notifications';
+import Bookmarks from './Bookmarks';
+import TweetDetails from './TweetDetails';
+import Profile from './Profile';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+            <HomeFeed />
+          </Route>
+          <Route exact path='/notifications'>
+            <Notifications />
+          </Route>
+          <Route exact path='/bookmarks'>
+            <Bookmarks />
+          </Route>
+          <Route exact path='/tweet/:tweetId'>
+            <TweetDetails />
+          </Route>
+          <Route exact path='/:profileId'>
+            <Profile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+};
+
+export default App;
