8230f06726eebac43acd1d4a2d00f5d0b538a90f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-15 13:02:05 -0400

    removed border-right from sidebar

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import catLogo from './assets/logo.svg';
-import { FiHome, FiUser, FiBell, FiBookmark } from 'react-icons/fi';
-import styled from 'styled-components/macro';
-import { NavLink } from 'react-router-dom';
-import { COLORS } from './constants';
-import { CurrentUserContext } from './CurrentUserContext';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import catLogo from "./assets/logo.svg";
+import { FiHome, FiUser, FiBell, FiBookmark } from "react-icons/fi";
+import styled from "styled-components/macro";
+import { NavLink } from "react-router-dom";
+import { COLORS } from "./constants";
+import { CurrentUserContext } from "./CurrentUserContext";
 
 const Sidebar = () => {
   const { currentUser } = React.useContext(CurrentUserContext);
 
   return (
     <Wrapper>
-      <CatLogo src={catLogo}/>
-      <NavigationLink exact to='/'>
-        <FiHome style={{verticalAlign:'middle'}}/>
+      <CatLogo src={catLogo} />
+      <NavigationLink exact to="/">
+        <FiHome style={{ verticalAlign: "middle" }} />
         <StyledSpan>Home</StyledSpan>
       </NavigationLink>
       <NavigationLink to={`/profile/${currentUser.profile.handle}`}>
-        <FiUser style={{verticalAlign:'middle'}}/>
+        <FiUser style={{ verticalAlign: "middle" }} />
         <StyledSpan>Profile</StyledSpan>
       </NavigationLink>
-      <NavigationLink to='/notifications'>
-        <FiBell style={{verticalAlign:'middle'}}/>
+      <NavigationLink to="/notifications">
+        <FiBell style={{ verticalAlign: "middle" }} />
         <StyledSpan>Notifications</StyledSpan>
       </NavigationLink>
-      <NavigationLink to='Bookmarks'>
-        <FiBookmark style={{verticalAlign:'middle'}}/>
+      <NavigationLink to="Bookmarks">
+        <FiBookmark style={{ verticalAlign: "middle" }} />
         <StyledSpan>Bookmarks</StyledSpan>
       </NavigationLink>
       <MeowBtn>MEOW</MeowBtn>
     </Wrapper>
-  )
+  );
 };
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-left:120px;
+  margin-left: 120px;
   text-align: left;
   margin-top: 45px;
-  border-right: 1px solid lightslategrey;
-  padding-right:10px;
-`
+  padding-right: 10px;
+`;
 
 const StyledSpan = styled.span`
   margin-left: 30px;
@@ -51,18 +49,18 @@ const StyledSpan = styled.span`
 
 const CatLogo = styled.img`
   box-sizing: border-box;
-  position:relative;
+  position: relative;
   width: 65px;
   left: -20px;
 `;
 
 const NavigationLink = styled(NavLink)`
   text-decoration: none;
-  display:flex;
+  display: flex;
   color: black;
   font-weight: bold;
   margin: 10px 0;
-  text-align:left;
+  text-align: left;
   font-size: 24px;
 
   &.active {
@@ -74,7 +72,7 @@ const MeowBtn = styled.button`
   background-color: ${COLORS.primary};
   color: white;
   border-radius: 20px;
-  border:none;
+  border: none;
   cursor: pointer;
 `;
 export default Sidebar;
